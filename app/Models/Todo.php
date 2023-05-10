90096340cba774e2a67da14d4fc58cf3a6d0039b
Watcharamet Srinethirothai <platoosom@gmail.com>
2023-05-10 09:48:56 +0700

    Add relation to TodoModel

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Todo extends Model
 {
     use HasFactory;
 
     protected $table = 'todoes';
+
+    protected $with = ['owner', 'assignto'];
+
+    public function owner()
+    {
+        return $this->hasOne(User::class, 'id', 'owner');
+    }
+    
+    public function assignto()
+    {
+        return $this->hasOne(User::class, 'id', 'assignto');
+    }
 }
