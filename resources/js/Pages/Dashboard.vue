a98a5c59f38e91b4d261a78d49717380285d6857
Watcharamet Srinethirothai <platoosom@gmail.com>
2023-05-12 12:06:05 +0700

    Define updated form

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -22,6 +22,8 @@ const props = defineProps({
 });
 
 const form = useForm({
+    mode: 'create',
+    id: '',
     tasktype: '',
     company: '',
     contact: '',
@@ -50,6 +52,7 @@ const store = reactive({
     assignto: null,
     duedate: null,
     reminder: null,
+    remindertime: null,
     priority: null,
     detail: null,
 })
@@ -60,45 +63,69 @@ const store = reactive({
 const onCheckedItem = (i) => {
 
     if(i.value <= 0){
-      clearStore()
+      store.checkedItem = null
       return
     }
 
-    setStore(i.value);
+    const id = i.value;
+    store.checkedItem = id;
 };
 const openFormModal = () => {
+    clearForm();
     store.openFormModal = true;
 };
 const closeModal = () => {
     store.openFormModal = false;
 };
+const openUpdatedFormModal = () => {
+    fillForm();
+    store.openFormModal = true;
+};
 const openDetailModal = () => {
+    fillStore();
     store.openDetailModal = true;
 };
 const closeDetailModal = () => {
     store.openDetailModal = false;
 };
-const openOKModal = () => {
-    store.openOKModal = true;
-};
 const closeOKModal = () => {
     store.openOKModal = false;
     store.openFormModal = false;
 };
-const clearStore = () => {
-    store.checkedItem = null 
-    store.tasktype = null 
-    store.company = null
-    store.contact = null
-    store.subject = null
-    store.assignto = null
-    store.duedate = null
-    store.reminder = null
-    store.priority = null
-    store.detail = null
-}
-const setStore = (id) => {
-    store.checkedItem = id
+const clearForm = () => {
+    form.id = null 
+    form.tasktype = null 
+    form.company = null 
+    form.contact = null 
+    form.subject = null 
+    form.assignto = null 
+    form.duedate = null 
+    form.duetime = null 
+    form.reminder = null 
+    form.remindertime = null 
+    form.priority = null 
+    form.detail = null  
+};
+const fillForm = () => {
+    const id = store.checkedItem 
+    const todo = props.todoes.find( (t) => t.id == id);
+
+    // Set store
+    form.id = todo.id
+    form.tasktype = todo.tasktype
+    form.company = todo.company
+    form.contact = todo.contact
+    form.subject = todo.subject
+    form.assignto = todo.assignto?.id 
+    form.duedate = todo.duedate
+    form.duetime = todo.duetime 
+    form.reminder = todo.reminder
+    form.remindertime = todo.remindertime 
+    form.priority = todo.priority
+    form.detail = todo.detail
+};
+const fillStore = () => {
+    const id = store.checkedItem 
     const todo = props.todoes.find( (t) => t.id == id);
 
     // Set store
@@ -106,9 +133,11 @@ const setStore = (id) => {
     store.company = todo.company
     store.contact = todo.contact
     store.subject = todo.subject
-    store.assignto = todo.assignto
+    store.assignto = todo.assignto?.id 
     store.duedate = todo.duedate
+    store.duetime = todo.duetime 
     store.reminder = todo.reminder
+    store.remindertime = todo.remindertime 
     store.priority = todo.priority
     store.detail = todo.detail
 }
@@ -124,6 +153,13 @@ const onInsertItem = () => {
         onError: () => {},
     });
 }
+
+/**
+ * Update Item
+ */
+const onUpdateItem = () => {
+
+} 
  
 
 /**
@@ -152,7 +188,7 @@ const onDeleteItem = () => {
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Todo Lists</h2>
         </template>
        
-        <div class="py-12"> 
+        <div class="py-12"> {{  todoes }}
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm ">
                     <div class="p-6 text-gray-900">
@@ -164,7 +200,7 @@ const onDeleteItem = () => {
                                   </svg>
                                 <span>New Task</span>
                               </button>
-                            <button :disabled="!store.checkedItem" class="disabled:opacity-25 disabled:cursor-not-allowed bg-neutral-100 hover:bg-neutral-300 text-gray-800 font-bold py-2 px-4 mx-2  inline-flex items-center">
+                            <button @click="openUpdatedFormModal" :disabled="!store.checkedItem" class="disabled:opacity-25 disabled:cursor-not-allowed bg-neutral-100 hover:bg-neutral-300 text-gray-800 font-bold py-2 px-4 mx-2  inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                                   </svg>
@@ -449,7 +485,7 @@ const onDeleteItem = () => {
                                   </div>
                                   <div class="md:w-2/3">
                                     <span class="block text-gray-500 md:text-left mb-1 md:mb-0 px-6 py-2">
-                                      {{ store.duedate }}
+                                      {{ store.duedate }} {{ store.duetime }}
                                     </span>
                                   </div>
                                 </div>                                  
@@ -461,7 +497,7 @@ const onDeleteItem = () => {
                                   </div>
                                   <div class="md:w-2/3">
                                     <span class="block text-gray-500 md:text-left mb-1 md:mb-0 px-6 py-2">
-                                      {{ store.reminder }}
+                                      {{ store.reminder }} {{  store.remindertime }}
                                     </span>
                                   </div>
                                 </div>                                  
